19caba16f7d47b4d344a6de1c21bde4ac5b141c7
NepaliSRK <nepalisrk@gmail.com>
2023-05-10 12:08:59 +0545

    After deployment

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello Skill Prompt, I'm Santosh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is the change I made after the deployment of site in vercel
       </p>
     </>
-  )
+  );
 }
 
 export default App
