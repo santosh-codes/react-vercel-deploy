dd0dccf93a1876cfdf8d258aaed6539ee6450f9b
NepaliSRK <nepalisrk@gmail.com>
2023-05-11 11:25:13 +0545

    second change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import deployLogo from "/deploy.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.skillprompt.com" target="_blank">
+          <img src={deployLogo} className="logo" alt="deploy" />
         </a>
       </div>
       <h1>Hello Skill Prompt, I'm Santosh</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+
       <p className="read-the-docs">
-        This is the change I made after the deployment of site in vercel
+        This change is made after the deployment of site in vercel
       </p>
     </>
   );
